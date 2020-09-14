80576cc6341df3d3d8216bc5a1059e08d1ba32e0
jariperttunen <32894586+jariperttunen@users.noreply.github.com>
2020-09-14 09:09:51 +0300

    Command line for XMLUpdate
    Command line program for Motti XMLUpdate Django www applications. Currently for localhost (127.0.0.1) 
    and shows how to bypass Valtori proxy server.

--- commandline_fertilization.py
+++ commandline_fertilization.py
@@ -1,12 +1,14 @@
 import argparse
 import requests
+import os
 from requests.auth import HTTPBasicAuth
 #1. Motti fertilization www addresses
-motti_address='https://motti.luke.fi/'
-url_home = motti_address
-url_upload = url_home+'fertilization/uploadtemplates'
-url_run = url_home+'fertilization/runfertilization'
-url_get = url_home+'fertilization/downloadexcel'
+server_address='http://127.0.0.1:8000/'
+url_home = server_address
+url_upload = url_home+'uploadxml'
+url_run = url_home+'updatexml'
+url_get = url_home+'downloadxml'
+proxies = {'http':'http://10.88.2.10:8080','https':'https://10.88.2.10:8080'}
 #2. This 'browser_header'emulates Safari on macOS as User-Agent.
 #   Django requires 'Referer' [sic] in the http header for all 'POST' requests.
 #   For more see https://en.wikipedia.org/wiki/User_agent. 
@@ -24,39 +26,33 @@ browser_header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6)
 #files_data = {'upload_template_files': (args.f, open(args.f, 'rb'), 'application/vnd.ms-excel')}
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f',type=str,dest='f',help="Excel template input file")
-    parser.add_argument('-u',type=str,dest='u',help="User")
-    parser.add_argument('-p',type=str,dest='p',help="Password")
+    parser.add_argument('-f',type=str,dest='f',help="XML template input file")
     args = parser.parse_args()
     if args.f == None:
-      print("No excel file")
+      print("No XML file")
       quit()
-    if args.u == None:
-      print("No user name")
-      quit()
-    if args.p == None:
-      print("No password")
-      quit()
-    files_data = {'upload_template_files': (args.f, open(args.f, 'rb'), 'application/vnd.ms-excel')}
+    os.environ['NO_PROXY']='127.0.0.1'
+    files_data = {'upload_xml': (args.f, open(args.f, 'rb'), 'application/xml')}
     #1. Start the session, note the same connection is used throughout the session 
     s = requests.Session()
     #Query the home page, you will get the csrftoken (one of Django www security component)
     print("HOME PAGE")
-    r=s.get(url_home,headers=browser_header,auth=HTTPBasicAuth(args.u, args.p))
-    #print("R HEADER",r.headers)
+    r=s.get(url_home,headers=browser_header)
+    print("R HEADER",r.headers)
     #print("R CONTENT",r.content)
-    #print("R COOKIES",r.cookies)
+    print("R COOKIES",r.cookies)
     #response.raise_for_status()
     print("HOME PAGE DONE")
     csrf_token=r.cookies['csrftoken']
     print("CSRF",csrf_token)
     #2. Upload the excel file, send back cookies, note the 'csrfmiddlewaretoken' for the Django www application, note 'auth' is needed each time
-    r1=s.post(url_upload,headers=browser_header,cookies=r.cookies,files=files_data,data={'csrfmiddlewaretoken':csrf_token},auth=HTTPBasicAuth(args.u, args.p))
+    r1=s.post(url_upload,headers=browser_header,cookies=r.cookies,files=files_data,data={'csrfmiddlewaretoken':csrf_token})
     #3. Run the motti simulation
-    r2=s.post(url_run,headers=browser_header,cookies=r.cookies,data={'csrfmiddlewaretoken':csrf_token},auth=HTTPBasicAuth(args.u, args.p))
+    r2=s.post(url_run,headers=browser_header,cookies=r.cookies,data={'csrfmiddlewaretoken':csrf_token})
     #4. Retrieve the resulting excel file
-    r3=s.get(url_get,headers=browser_header,cookies=r.cookies,data={'csrfmiddlewaretoken':csrf_token},auth=HTTPBasicAuth(args.u, args.p))
-    #4.1 Fancy way to retrieve the file name, you should get the same uploaded file but with results, with "_result.xlsx" in the name.
+    r3=s.get(url_get,headers=browser_header,cookies=r.cookies,data={'csrfmiddlewaretoken':csrf_token})
+    #4.1 Fancy way to retrieve the file name, you should get the same uploaded file but with results, with "Out.xml" in the name.
+    print("r3 HEADERS",r3.headers)
     fname = r3.headers['Content-Disposition'].split('=')[1]
     #4.2 Write the file
     f=open(fname, 'wb')
